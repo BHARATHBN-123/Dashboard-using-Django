bbf5ed726ca1b48f69d55e375799f80cb0f12f8a
BHARATH B N <bharathbn17498@gmail.com>
2023-01-14 22:11:22 +0530

    added templates

--- DashboardWebsite/dashboardapp/views.py
+++ DashboardWebsite/dashboardapp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+import requests
 
 # Create your views here.
+def get_products():
+    url = 'https://fakestoreapi.com/products'
+    r = requests.get(url , auth=True)
+    products = r.json() 
+    print (products)
+    product_list = []
+    for i in products:
+        product_list.append(i)
+    return product_list
+get_products()
---
+++ DashboardWebsite/test.py
@@ -0,0 +1,12 @@
+import requests
+
+
+url = 'https://fakestoreapi.com/products'
+r = requests.get(url )
+products = r.json()
+
+product_list = []
+for i in products:
+    product_list.append(i)
+
+    
