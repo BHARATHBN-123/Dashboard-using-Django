1d79d8e1696c810305310a1fdfc3386062d59b8a
BHARATH B N <bharathbn17498@gmail.com>
2023-01-17 18:00:26 +0530

    dashboard template installation

--- DashboardWebsite/dashboardapp/views.py
+++ DashboardWebsite/dashboardapp/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 import requests
 from django.core.paginator import Paginator
 
-def display_products(request,*args,**kwargs):
+
+def display_products(request, *args, **kwargs):
     url = 'https://fakestoreapi.com/products'
     r = requests.get(url)
-    products = r.json() 
-    Paginator = Paginator(products , 3)
-    return render (request , 'dashboardapp/dashboard.html',{'products': products})
+    products = r.json()
+    # Paginator = Paginator(products , 3)
+    return render(request, 'dashboardapp/dashboard.html', {'products': products})
